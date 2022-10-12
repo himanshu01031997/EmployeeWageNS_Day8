dc7783442a1f0f2d7dfabb14b76b49b879f673fe
himanshu01031997 <hs010397@gmail.com>
2022-10-12 23:13:00 +0530

    added attendance

---
+++ EmployeewageDay8/Attendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeewageDay8
+{
+    public class Attendance
+    {
+        public  int ispresent = 1;
+        public  void attend()
+        {
+            Console.WriteLine("Attendance of employee");
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == 1)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
