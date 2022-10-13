d54e6ceeaeb69497fb2073a621b934af4d973b38
himanshu01031997 <hs010397@gmail.com>
2022-10-13 16:57:02 +0530

    total emp wage

--- EmployeewageDay8/Program.cs
+++ EmployeewageDay8/Program.cs
@@ -24,10 +24,17 @@
             //Refactoring code = new Refactoring();
             //code.monthlywages();
 
-            MultipleCompany company1 = new MultipleCompany();
-            company1.wages("Dmart", 20, 14, 40);
-            MultipleCompany company2 = new MultipleCompany();
-            company2.wages("jio", 23, 13, 50);
+            //MultipleCompany company1 = new MultipleCompany();
+            //company1.wages("Dmart", 20, 14, 40);
+            //MultipleCompany company2 = new MultipleCompany();
+            //company2.wages("jio", 23, 13, 50);
+
+            TotalWages total = new TotalWages("Dmart", 20, 15, 23);
+            total.ComputeEmpWage();
+            Console.WriteLine(total.toString());  
+            //TotalWages total1 = new TotalWages("jio", 21, 25, 13);
+            //total1.ComputeEmpWage();
+            //Console.WriteLine(total1.ToString());
 
 
 
